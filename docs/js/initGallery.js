f35c345b988595ef6e870c793b036072e48b8083
Thales Lima Oliveira <thaleslima.ufu@gmail.com>
2020-07-15 17:04:33 -0300

    Update initGallery.js

--- docs/js/initGallery.js
+++ docs/js/initGallery.js
@@ -16,7 +16,7 @@ $(function () {
   // Initialize the Gallery as video carousel:
   // eslint-disable-next-line new-cap
   blueimp.Gallery(document.getElementById('links').getElementsByTagName('a'), {
-    container: '#__docusaurus > #blueimp-gallery-carousel',
+    container: '#__docusaurus div div #blueimp-gallery-carousel',
     carousel: true
   })
 })
